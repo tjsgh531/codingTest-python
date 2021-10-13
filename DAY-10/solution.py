c2951c5215cc0c80ca8f0a7726181d4aa6d79e7a
Sun Ho <tjsgh5312@daum.net>
2021-10-13 15:06:24 +0900

    imcomplete DAY-10

--- DAY-10/solution.py
+++ DAY-10/solution.py
@@ -1,25 +1,29 @@
 import datetime
-import enum
 import re
+import time
 
 def solution(lines):
     lineslist = [line.split() for line in lines]
     callineslist = []
     for value in lineslist:
-        end = datetime.datetime.strptime(value[0]+value[1],"%Y-%m-%d%H:%M:%S.%f")
-
+        end = time.mktime(datetime.datetime.strptime(value[0]+value[1],"%Y-%m-%d%H:%M:%S.%f").timetuple())
         taketime = float(re.sub('s$','', value[-1]))
-        start = end - datetime.timedelta(seconds=taketime) + datetime.timedelta(seconds=0.001)
+        start = end - float(datetime.timedelta(seconds=taketime).total_seconds()) + float(datetime.timedelta(seconds=0.001).total_seconds())
         callineslist.append((start, end))
 
-    time = callineslist[0][0] 
-    lasttime = callineslist[-1][-1]
+    for value in callineslist:
+        print (datetime.datetime.fromtimestamp(value[-1]))
 
-    maxonprocess = 1
-    for index,value in enumerate(callineslist) :
-        if index == 0 : continue
-        for check in range(index-1) :
-            if value[0] < callineslist(check)[-1]:
-                if index+1
-    
-    return maxonprocess
+
+    maxonprocessnum = 0
+
+    for value in callineslist:
+        
+        starttime = value[0]
+        count = 0
+        for val in callineslist:
+            if starttime <= val[0] <= starttime + 1.0: count += 1 
+            elif starttime < val[0] : break
+        if maxonprocessnum < count : maxonprocessnum = count
+
+    return maxonprocessnum
