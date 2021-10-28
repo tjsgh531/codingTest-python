5164a4cb8da5549e2b23e8c02dacd7d47c8b4cdf
Sun Ho <tjsgh5312@daum.net>
2021-10-28 10:55:20 +0900

    "complete Day-14 by using stack" "But DAY -15 is too difficult to solve as soon as seen(I need more time)"

---
+++ DAY-15/solution.py
@@ -0,0 +1,20 @@
+def solution(rows, columns, queries):
+    # ((i-1) x columns + j)
+    bord = []
+    for r in range(rows):
+        temp = []
+        for c in range(columns):
+            temp.append = (r-1) * columns + c
+        bord.append(temp)
+    
+    
+    answer = []
+    return answer
+
+def rotateBord(list1):
+    row1 = list1[0]
+    row2 = list1[2]
+    col1 = list1[1]
+    col2 = list1[3]
+
+    heap? hash??
