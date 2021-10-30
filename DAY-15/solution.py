6fbeb0f80b97ce6e6da99be71cfa01c691641b01
Sun Ho <tjsgh5312@daum.net>
2021-10-30 10:03:59 +0900

    complete DAY-15
    - 2차원 리스트 (복사, 탐색, 수정)
    - stack 활용

--- DAY-15/solution.py
+++ DAY-15/solution.py
@@ -1,20 +1,36 @@
+import copy
 def solution(rows, columns, queries):
-    # ((i-1) x columns + j)
-    bord = []
-    for r in range(rows):
-        temp = []
-        for c in range(columns):
-            temp.append = (r-1) * columns + c
-        bord.append(temp)
     
-    
+    bord = [ [(r-1) * columns + c for c in range(1,columns+1)] for r in range(1, rows+1)]
     answer = []
+
+    for y1, x1, y2, x2 in queries:
+        minimum = None
+        temp = copy.deepcopy(bord)
+        
+        for i in range(x1, x2):
+            if minimum is None or minimum > bord[y1-1][i]: minimum = bord[y1-1][i]
+            bord[y1-1][i] = temp[y1-1][i-1]
+
+
+        for i in range(y1,y2):
+            minimum = min(minimum,bord[i][x2-1])
+            bord[i][x2-1] = temp[i-1][x2-1] 
+
+   
+        for i in range(x1-1, x2-1):
+            minimum = min(minimum,bord[y2-1][i])
+            bord[y2-1][i] = temp[y2-1][i+1] 
+
+        
+        for i in range(y1-1,y2-1):
+            minimum = min(minimum, bord[i][x1-1])
+            bord[i][x1-1] = temp[i+1][x1-1]
+
+        
+        answer.append(minimum)
+        print(minimum)
+
     return answer
 
-def rotateBord(list1):
-    row1 = list1[0]
-    row2 = list1[2]
-    col1 = list1[1]
-    col2 = list1[3]
-
-    heap? hash??
+solution(3,3,[[1, 1, 2, 2], [1, 2, 2, 3], [2, 1, 3, 2], [2, 2, 3, 3]])
